5861eac2584e44298e6c17341548916f84151f30
SahlaSherin <sahlasherin734@gmail.com>
2026-07-05 22:08:28 +0530

    first commit

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,11 @@
+//centeralized API setup
+
+import axios from "axios";
+import qs from "qs";
+const api = axios.create({
+  baseURL: "http://localhost:8080/api",
+  withCredentials: true,
+  paramsSerializer: (params) => qs.stringify(params, { arrayFormat: "repeat" }),
+});
+
+export default api;
